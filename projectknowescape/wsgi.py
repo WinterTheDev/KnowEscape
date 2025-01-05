db891d237f885dd447386364ba1c739ca65fcf94
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-01-05 23:18:06 +0200

    removed manual database migration function

--- projectknowescape/wsgi.py
+++ projectknowescape/wsgi.py
@@ -8,16 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectknowescape.settings')
 
 application = get_wsgi_application()
 
-try:
-    call_command('apply_migrations')
-except Exception as e:
-    print(f"Error applying migrations: {e}")
 # vercel prod variable
 app = application
